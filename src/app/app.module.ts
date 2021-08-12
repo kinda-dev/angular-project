c3cda2e908f497851705f2c2e498c92da0ad3dd5
kinda-dev <maybe.fabio@gmail.com>
2021-08-12 11:39:45 -0700

    implement router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeroesItemComponent } from './components/heroes-item/heroes-item.component';
+
+const appRoutes: Routes = [
+  // {path: '', component: HeaderComponent},
+  {path: '', component: HeroesComponent}
+  // {path: '', component: FooterComponent}
+]
 
 @NgModule({
   declarations: [
@@ -18,7 +25,8 @@ import { HeroesItemComponent } from './components/heroes-item/heroes-item.compon
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
