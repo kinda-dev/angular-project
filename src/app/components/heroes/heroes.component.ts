f18734972b128c2842d37a7009849f7cd38950b5
kinda-dev <maybe.fabio@gmail.com>
2021-08-18 18:33:51 -0700

    clean code

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -19,54 +19,22 @@ export class HeroesComponent implements OnInit {
   heroes: HEROOBJ[] = [];
   showPrev: boolean = false;
   isFetching: boolean = true;
-  heroes$: Observable<any> = this.heroService.heroes$.pipe(
-    tap(data => console.log(data))
-  );
+  heroes$: Observable<any> = this.heroService.heroes$
+  // .pipe(
+  //   tap(data => console.log('what this', data))
+  // );
 
   
-  testHeroes$ :HEROOBJ[] | any = []
 
   
   constructor(
     private heroService: HeroService,
-    private store: Store<{heroes: HEROOBJ}>,
-    // below is correct? 
-    private _store: Store<IAppState>,
-    private _state: State<IAppState>
+    private _store: Store<IAppState>
     ) {}
 
   ngOnInit(): void {
-    // below for the store
     this._store.dispatch(new GetHeroes());
-    console.log('HEROES:', this.heroes$)
-    console.log('STORE!!!!', this._store)
-    console.log('state!!!!', this._state)
-    // this.test = this.store.select('heroes')
-    console.log('STORE', this.store.select('heroes'))
-    this.heroService.getHeroes().subscribe((payload) => {
-      this.isFetching = false;
-      this.heroes = payload.data.results;
-    });
-
-  }
-
-  getOtherHeroes(action: string): void {
-    this.isFetching = true;
-    let elementsToGet: number = 50;
-
-    if (action === 'Prev') {
-      elementsToGet = -50;
-    } 
-
-    this.heroService.getHeroes(elementsToGet).subscribe((payload) => {
-      this.isFetching = false;
-      this.heroes = payload.data.results;
-      if (payload.data.offset > 0) {
-        this.showPrev = true;
-      } else {
-        this.showPrev = false;
-      }
-    });
+    this.isFetching = false;
   }
 
   testFunc(): void {
--- src/app/store/effects/hero.effects.ts
+++ src/app/store/effects/hero.effects.ts
@@ -11,7 +11,6 @@ import {
     GetHeroes 
 } from "../actions/heroes.actions";
 import { HeroService } from "src/app/services/hero.service";
-import { HEROOBJ } from "src/app/interfaces/Hero";
 import { selectHeroes } from "../selectors/hero.selector";
 
 //testing import below
@@ -20,48 +19,23 @@ import { config } from "src/config";
 import { HttpClient } from "@angular/common/http";
 
 @Injectable()
-// export class HeroEffetcs {
-//     // between comments testing
-//     private md5 = new Md5();
-//     private timeStamp = Date.now().toString();
-//     private apiKey = config.PUBLIC_KEY;
-//     private privateKey = config.PRIVATE_KEY;
-//     private md5hash = this.md5.appendStr(this.timeStamp+this.privateKey+this.apiKey).end();
-//     private entriesLimit = 50;
-//     private apiUrl = `https://gateway.marvel.com/v1/public/characters?limit=${this.entriesLimit}&ts=${this.timeStamp}&apikey=${this.apiKey}&hash=${this.md5hash}`;
-//     offset = 75;
-//     //------------------------------------------------
-//     getHero$ = createEffect(() => this._actions$.pipe(
-//         ofType<GetHeroes>(EHeroesActions.GetHeroes),
-//         // switchMap(() => this._heroService.getHeroes()),
-//         switchMap(() => this.http.get(this.apiUrl + "&offset=" + this.offset)),
-//         switchMap((heroHttp: HEROOBJ) => of(new GetHeroesSuccess(heroHttp)))
-//     ))
-//     constructor(
-//         private _heroService: HeroService,
-//         private _actions$: Actions,
-//         private _store: Store<IAppState>,
-//         private http:HttpClient,
-//     ) {}
-// }
+
 
 export class HeroEffetcs {
-    // between comments testing
     private md5 = new Md5();
     private timeStamp = Date.now().toString();
     private apiKey = config.PUBLIC_KEY;
     private privateKey = config.PRIVATE_KEY;
     private md5hash = this.md5.appendStr(this.timeStamp+this.privateKey+this.apiKey).end();
-    private entriesLimit = 50;
+    private entriesLimit = 25;
     private apiUrl = `https://gateway.marvel.com/v1/public/characters?limit=${this.entriesLimit}&ts=${this.timeStamp}&apikey=${this.apiKey}&hash=${this.md5hash}`;
-    offset = 75;
-    //------------------------------------------------
+    offset = 0;
     getHero$ = createEffect(() => this._actions$.pipe(
         ofType<GetHeroes>(EHeroesActions.GetHeroes),
         withLatestFrom(this._store.pipe(select(selectHeroes))),
-        // switchMap(() => this._heroService.getHeroes()),
         switchMap(() => this.http.get(this.apiUrl + "&offset=" + this.offset)),
         switchMap((heroHttp: any) => {
+            this.offset += 25;
             console.log(heroHttp.data);
             return of(new GetHeroesSuccess(heroHttp.data.results))
         }
